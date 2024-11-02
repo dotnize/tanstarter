a484bce4ca60a5cb4d42d1e809649366eb4e1e9e
Nathaniel Tampus <nate@mugnavo.com>
2024-11-02 13:57:44 +0800

    update db schema

--- app/server/db/schema.ts
+++ app/server/db/schema.ts
@@ -3,8 +3,6 @@ import { integer, pgTable, primaryKey, text, timestamp } from "drizzle-orm/pg-co
 export const user = pgTable("user", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: text(),
-  first_name: text(),
-  last_name: text(),
   avatar_url: text(),
   email: text().unique().notNull(),
 
@@ -25,9 +23,7 @@ export const oauthAccount = pgTable(
       .notNull()
       .references(() => user.id),
   },
-  (table) => ({
-    pk: primaryKey({ columns: [table.provider_id, table.provider_user_id] }),
-  }),
+  (table) => [primaryKey({ columns: [table.provider_id, table.provider_user_id] })],
 );
 
 export const session = pgTable("session", {
@@ -40,8 +36,6 @@ export const session = pgTable("session", {
     mode: "date",
   }).notNull(),
 });
-
-// TODO: tables for payment gateways
 
 export type User = typeof user.$inferSelect;
 export type Session = typeof session.$inferSelect;
