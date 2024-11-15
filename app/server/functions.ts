6096ab4fcf5c9127aedf0f70efda2db907c07cab
Nathaniel Tampus <nate@mugnavo.com>
2024-11-15 10:17:04 +0800

    fix server fn return

--- app/server/functions.ts
+++ app/server/functions.ts
@@ -1,7 +1,7 @@
-import { createServerFn, json } from "@tanstack/start";
+import { createServerFn } from "@tanstack/start";
 import { getAuthSession } from "~/server/auth";
 
 export const getUser = createServerFn({ method: "GET" }).handler(async () => {
   const { user } = await getAuthSession();
-  return json(user);
+  return user;
 });
