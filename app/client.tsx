4131a071d4855303aa6c16528275609a0b49a09c
Nathaniel Tampus <nate@mugnavo.com>
2024-11-16 01:03:01 +0800

    remove check for root element

--- app/client.tsx
+++ app/client.tsx
@@ -5,9 +5,4 @@ import { createRouter } from "./router";
 
 const router = createRouter();
 
-const root = document.getElementById("root");
-if (!root) {
-  throw new Error("Root element not found");
-}
-
-hydrateRoot(root, <StartClient router={router} />);
+hydrateRoot(document, <StartClient router={router} />);
