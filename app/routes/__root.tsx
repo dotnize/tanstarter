39e73cf2a14568fd89ced833acc48178d1a7945a
Nathaniel Tampus <nate@mugnavo.com>
2024-11-18 07:11:21 +0800

    use new `head` method

--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -31,20 +31,22 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
     const user = await getUser();
     return { user };
   },
+  head: () => ({
+    meta: [
+      {
+        charSet: "utf-8",
+      },
+      {
+        name: "viewport",
+        content: "width=device-width, initial-scale=1",
+      },
+      {
+        title: "TanStarter",
+      },
+    ],
+    links: [{ rel: "stylesheet", href: appCss }],
+  }),
   component: RootComponent,
-  meta: () => [
-    {
-      charSet: "utf-8",
-    },
-    {
-      name: "viewport",
-      content: "width=device-width, initial-scale=1",
-    },
-    {
-      title: "TanStarter",
-    },
-  ],
-  links: () => [{ rel: "stylesheet", href: appCss }],
 });
 
 function RootComponent() {
