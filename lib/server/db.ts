15241253104de9893a90b47e0451dcfb6aea17d7
Nathaniel Tampus <nate@mugnavo.com>
2025-01-07 13:31:47 +0800

    cleanup exports

--- lib/server/db.ts
+++ lib/server/db.ts
@@ -5,8 +5,3 @@ import * as schema from "./schema";
 
 const driver = postgres(process.env.DATABASE_URL as string);
 export const db = drizzle({ client: driver, schema });
-
-export const table = schema;
-
-// You can export your schema types here
-export type User = typeof schema.user.$inferSelect;
