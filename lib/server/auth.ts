742cba62e46a5994156ef8a83ee4e4fc3bb08534
Nathaniel Tampus <nate@mugnavo.com>
2025-02-08 18:23:00 +0800

    enable session cookie cache by default

--- lib/server/auth.ts
+++ lib/server/auth.ts
@@ -9,6 +9,14 @@ export const auth = betterAuth({
     provider: "pg",
   }),
 
+  // https://www.better-auth.com/docs/concepts/session-management#session-caching
+  session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60, // 5 minutes
+    },
+  },
+
   // https://www.better-auth.com/docs/concepts/oauth
   socialProviders: {
     github: {
@@ -28,13 +36,5 @@ export const auth = betterAuth({
   // https://www.better-auth.com/docs/authentication/email-password
   // emailAndPassword: {
   //   enabled: true,
-  // },
-
-  // https://www.better-auth.com/docs/concepts/session-management#session-caching
-  // session: {
-  //   cookieCache: {
-  //     enabled: true,
-  //     maxAge: 5 * 60, // 5 minutes
-  //   },
   // },
 });
