cec1e1562ec391a2bf8900d02dd7ee9dc6ac42c8
Nathaniel Tampus <nate@mugnavo.com>
2025-02-15 11:55:54 +0800

    regen auth schema

--- lib/server/schema/auth.schema.ts
+++ lib/server/schema/auth.schema.ts
@@ -20,7 +20,7 @@ export const session = pgTable("session", {
   userAgent: text("user_agent"),
   userId: text("user_id")
     .notNull()
-    .references(() => user.id),
+    .references(() => user.id, { onDelete: "cascade" }),
 });
 
 export const account = pgTable("account", {
@@ -29,7 +29,7 @@ export const account = pgTable("account", {
   providerId: text("provider_id").notNull(),
   userId: text("user_id")
     .notNull()
-    .references(() => user.id),
+    .references(() => user.id, { onDelete: "cascade" }),
   accessToken: text("access_token"),
   refreshToken: text("refresh_token"),
   idToken: text("id_token"),
