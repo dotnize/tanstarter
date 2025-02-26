413b451a7c0be64bd0d46b3c96b5021bdf0895ba
Jacky Tea <44061647+JackyTea@users.noreply.github.com>
2025-02-25 20:19:17 -0500

    chore(app): migrate @tanstack/start to @tanstack/react-start

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "@tanstack/start/config";
+import { defineConfig } from "@tanstack/react-start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
