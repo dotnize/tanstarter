030fbda3c26bf2dc37f0443522397dc8effd44d2
Ishiyama Yuzuki <yuzupon1480@gmail.com>
2025-03-29 18:03:40 +0900

    format

--- src/routes/dashboard/index.tsx
+++ src/routes/dashboard/index.tsx
@@ -8,7 +8,7 @@ function DashboardIndex() {
   return (
     <div className="flex flex-col gap-1">
       Dashboard index page
-      <pre className="rounded-md border bg-card p-1 text-card-foreground">
+      <pre className="bg-card text-card-foreground rounded-md border p-1">
         routes/dashboard/index.tsx
       </pre>
     </div>
