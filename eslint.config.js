e654c0f7783765217fa97fc68c67db869882becd
Nathaniel Tampus <nate@mugnavo.com>
2025-05-06 15:52:12 +0800

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -6,36 +6,28 @@ import eslintConfigPrettier from "eslint-config-prettier";
 import * as reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  {
-    ignores: ["dist", ".vinxi", ".wrangler", ".vercel", ".netlify", ".output", "build/"],
-  },
-  {
-    files: ["**/*.{ts,tsx}"],
-    extends: [
-      js.configs.recommended,
-      ...tseslint.configs.recommended,
-      eslintConfigPrettier,
-      ...pluginQuery.configs["flat/recommended"],
-      ...pluginRouter.configs["flat/recommended"],
-    ],
-  },
-  reactHooks.configs.recommended,
-  {
-    files: ["**/*.{ts,tsx}"],
-    languageOptions: {
-      parserOptions: {
-        parser: tseslint.parser,
-        project: "./tsconfig.json",
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    ...react.configs["recommended-type-checked"],
-  },
-  {
-    rules: {
-      // You can override any rules here
-      "react-hooks/react-compiler": "warn",
+export default tseslint.config({
+  ignores: ["dist", ".vinxi", ".wrangler", ".vercel", ".netlify", ".output", "build/"],
+  files: ["**/*.{ts,tsx}"],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      projectService: true,
+      tsconfigRootDir: import.meta.dirname,
     },
   },
-);
+  extends: [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    eslintConfigPrettier,
+    ...pluginQuery.configs["flat/recommended"],
+    ...pluginRouter.configs["flat/recommended"],
+    reactHooks.configs.recommended,
+    react.configs["recommended-type-checked"],
+    // ...you can add plugins or configs here
+  ],
+  rules: {
+    // You can override any rules here
+    "react-hooks/react-compiler": "warn",
+  },
+});
