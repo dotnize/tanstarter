9d797de11e7840a8999f0b64d90ef680951dd15a
Masum Gökyüz <masumgokyuz@gmail.com>
2025-07-01 16:24:11 +0300

    feat: type-safe env. variables

---
+++ src/env/client.ts
@@ -0,0 +1,10 @@
+import { createEnv } from "@t3-oss/env-core";
+import { z } from "zod";
+
+export const env = createEnv({
+  clientPrefix: "VITE_",
+  client: {
+    VITE_BASE_URL: z.string().url().default("http://localhost:3000"),
+  },
+  runtimeEnv: import.meta.env,
+});
